b3fe5fa4eefd122802ee45d7cde00e28fc6af09b
Magomedcoder <info@magomedcoder.ru>
2022-11-14 23:41:36 +0300

    first commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,15 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Magomedcoder on 14.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {}
+    }
+}
